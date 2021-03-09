8e0b88c32921c7984fce502f8e2d0cd3d577e361
Deokti <reilgelern@mail.ru>
2021-03-09 18:41:09 +0300

    Добавил новый компонент, который принимает разные пропсы и создаёт кнопку разной формы. Компонент поможет заметить две кнопки ButtonBack и Help со стилями и логикой одним передам нужные данные.

---
+++ dieedid-vue-spa/src/components/repeat/Button.vue
@@ -0,0 +1,77 @@
+<template>
+  <button
+    class="v-button"
+    v-bind:style="{
+      width: this.width,
+      height: this.height,
+      color: this.color,
+      fontSize: this.fontSize,
+      fontWeight: this.fontWeight,
+      textTransform: this.textTransform,
+      backgroundColor: this.backgroundColor,
+      border: this.border,
+      borderColor: this.borderColor,
+      borderRadius: this.borderRadius,
+      borderStyle: this.borderColor,
+      borderWidth: this.borderWidth,
+      cursor: this.cursor
+    }"
+  >
+    <div class="v-button-body">
+      <div
+        class="v-button-icon"
+        v-bind:style="{
+          width: this.iconWidth,
+          height: this.iconHeight,
+        }"
+      >
+        <slot name="icon" />
+      </div>
+      <slot name="label">Текст</slot>
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    width: String,
+    height: String,
+    color: String,
+    fontSize: String,
+    textTransform: String,
+    fontWeight: String,
+    backgroundColor: String,
+    border: String,
+    borderRadius: String,
+    borderColor: String,
+    borderWidth: String,
+    borderStyle: String,
+    iconWidth: String,
+    iconHeight: String,
+    cursor: {
+      type: String,
+      default: "pointer"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-button:focus {
+  outline: none;
+}
+.v-button-body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.v-button-icon {
+  margin-right: 10px;
+}
+.v-button-icon > * {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- dieedid-vue-spa/src/components/screens/PhysicalActivity.vue
+++ dieedid-vue-spa/src/components/screens/PhysicalActivity.vue
@@ -5,6 +5,7 @@
         <h2 class="physical__activity--title">Физическая активность</h2>
         <p class="physical__activity--descr">Исключая тренировки</p>
         <div class="physical__activity-button--wrapper">
+
           <ButtonBack />
           <div class="physical__activity--buttons">
             <div
@@ -16,9 +17,53 @@
               {{ act.name }}
             </div>
           </div>
+
           <Help />
+
         </div>
       </div>
+    </div>
+    
+    <!--  ОДИН И ТОТ ЖЕ КОМПОНЕНТ, НО С РАЗНЫМИ ПРОПСАМИ   -->
+    <!--  ПОСМОТРИШЬ, МОЖНО ЛИ ТАК ПИСАТЬ В САМОМ VUE, ИЛИ ЛЕДУЕТ ДУБЛИРОВАТЬ PROPS В DATA() {}  -->
+    <!--  ПОСКОЛЬКУ Я ДАВНО НЕ ПИСАЛ НА VUE, ДАЖЕ ОЧЕНЬ ДАВНО  -->
+    <div style="display: flex; margin: 20px; justify-content: center">
+      <!--  ПЕРВЫЙ КОМПОНЕНТ  -->
+      <Button
+        width="140px"
+        height="50px"
+        color="#fff599"
+        font-size="1.3333rem"
+        background-color="transparent"
+        text-transform="lowercase"
+        border-radius="25px"
+        border="1px solid #fff599"
+        icon-height="22px"
+        icon-width="22px"
+      >
+        <svg class="button-back__img" slot="icon">
+          <use xlink:href="../../assets/images/sprite/sprite.svg#arrow-back"></use>
+        </svg>
+        <span slot="label">назад</span>
+      </Button>
+
+      <!--  ВТОРОЙ КОМПОНЕНТ  -->
+      <Button
+        width="130px"
+        height="50px"
+        color="#fff599"
+        font-size="1.7222rem"
+        background-color="rgba(26, 23, 23, 0.5)"
+        border-radius="15px"
+        border="1px solid #fff599"
+        icon-height="36px"
+        icon-width="36px"
+      >
+        <svg class="button-back__img" slot="icon">
+          <use xlink:href="../../assets/images/sprite/sprite.svg#question"></use>
+        </svg>
+        <span slot="label">Help</span>
+      </Button>
     </div>
   </section>
 </template>
@@ -26,9 +71,12 @@
 <script>
 import ButtonBack from "@/components/repeat/ButtonBack.vue";
 import Help from "@/components/repeat/Help.vue";
+import Button from "@/components/repeat/Button";
+
 export default {
   name: "PhysicalActivity",
   components: {
+    Button,
     ButtonBack,
     Help
   },
@@ -54,7 +102,7 @@ export default {
 
 <style lang="scss">
 .physical__activity {
-  font-family: "Roboto Condensed";
+  font-family: "Roboto Condensed", sans-serif;
   text-align: center;
   color: $gold;
   text-transform: uppercase;
@@ -65,23 +113,28 @@ export default {
     background-size: contain;
     background-repeat: no-repeat;
   }
+
   &--title {
     font-size: 4.0556rem;
     margin: 0 0 20px;
   }
+
   &--descr {
     font-size: 1.3333rem;
     margin: 0 0 55px;
   }
 }
+
 .physical__activity-button--wrapper {
   display: flex;
   justify-content: space-between;
   padding: 0 30px;
 }
+
 .physical__activity--buttons {
   width: 800px;
 }
+
 .activity--button {
   color: $active-btn-color;
   background-color: $white;
@@ -95,6 +148,7 @@ export default {
   font-weight: 800;
   font-size: 1.6667rem;
   line-height: 2.2222rem;
+
   &:hover {
     background-color: $hover-btn-color;
     color: $white;
